febf4a67b0cd86009d417c69785161b7099d9e42
Alexander Martin <Alexander@theAlexanderMartin.com>
2020-03-28 23:51:43 -0400

    Ahhh! Wrong schema I put the wrong schema in the database.

--- Fishing/settings.py
+++ Fishing/settings.py
@@ -86,7 +86,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': "wrecks",
+            'NAME': "fish",
             'USER': os.getenv("DATABASE_USER"),
             'PASSWORD': os.getenv("DATABASE_PASSWORD"),
             'HOST': os.getenv("DATABASE_HOST"),
