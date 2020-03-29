cadecf25afc8993ee8fe51d8fd7a0f732385ffa5
Alexander Martin <Alexander@theAlexanderMartin.com>
2020-03-29 16:08:14 -0400

    Added Support for the Southern Hemisphere You can toggle between the northern and southern hemisphere now.

--- Fishing/settings.py
+++ Fishing/settings.py
@@ -145,21 +145,22 @@ LOGOUT_REDIRECT_URL = '/'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': '/home/ubuntu/WhatCanICatch/django.log',
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'filename': '/home/ubuntu/WhatCanICatch/django.log',
+            },
         },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
+        'loggers': {
+            'django': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
         },
-    },
-}
+    }
